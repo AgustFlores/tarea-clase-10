435437801a36086d80a362e71a5794e05fa54d48
AgustFlores <140025789+AgustFlores@users.noreply.github.com>
2023-08-27 20:15:42 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,22 @@
+var nombre  = "Agustin"   
+var apellido = "Flores"
+
+console.log (nombre + " " + apellido);
+
+// suma
+
+let numero1 = 10
+let numero2 = 23
+
+let result = numero1 + numero2
+
+console.log (result);
+
+let numero3 = 20
+let numero4 = result + numero3
+
+console.log (numero3 + result);
+
+
+
+
